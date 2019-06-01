7a51cea84e9415fa4d3aedb49510d19b89a01943
Spectre <antoine@doussaud.org>
2019-06-01 18:41:01 +0200

    full

--- 1_blink/main.c
+++ 1_blink/main.c
@@ -6,9 +6,7 @@
 int	main(void) {
 	DDRB |= (1 << PIN5); // OUTPUT
 	for(;;) {
-		PORTB |= (1 << PIN5); // HIGH
-		_delay_ms(100);
-		PORTB &= ~(1 << PIN5); // LOW
-		_delay_ms(100);
+		PORTB ^= (1 << PIN5);
+		_delay_ms(500);
 	}
 }
--- 2_uart/main.c
+++ 2_uart/main.c
@@ -1,5 +1,7 @@
-#define UART_BAUDRATE 9600
-#define BAUD_PRESCALE ((F_CPU / 16 / UART_BAUDRATE) - 1) // Async normal        20.3.1
+#define UART_BAUDRATE 115200
+
+// #define BAUD_PRESCALE ((F_CPU / 16 / UART_BAUDRATE) - 1) // Async normal     20.3.1
+#define BAUD_PRESCALE ((F_CPU / 8 / UART_BAUDRATE) - 1) // Async double speed   20.3.1
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -8,8 +10,9 @@ void uart_init(void) {
 	UBRR0L = BAUD_PRESCALE; // Set baud rate                                    20.11.5 UBRRnL
 	UBRR0H = BAUD_PRESCALE >> 8;
 
-	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00); // 9600/8-N-1                           20.11.4 UCSRnC
+	UCSR0A = 1<<U2X0; // Double speed                                           20.11.2 UCSRnA
 	UCSR0B = (1<<RXEN0)  | (1<<TXEN0);  // Enable receiver and transmitter      20.11.3 UCSRnB
+	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00); // 8-N-1                                20.11.4 UCSRnC
 }
 
 void uart_tx(unsigned char data) {
@@ -23,16 +26,17 @@ uint8_t uart_rx(void) {
 }
 
 void put_str(char* str, uint8_t size) {
-	for (uint8_t i=0; i<size; i++)
-		uart_tx(str[i]);
+	do
+		uart_tx(*str++);
+	while(--size);
 }
 
 int	main(void) {
-	char str[14] = "Hello World!\n";
+	char str[] = "Hello World!\r\n";
 
 	uart_init();
 	for(;;) {
-		put_str(str, 13);
-		_delay_ms(500);
+		put_str(str, sizeof(str)-1);
+		_delay_ms(1000);
 	}
 }
